61b2c69f07813b54e5ae785f963750440a53eba2
hsj3925 <hsj3925@naver.com>
2023-03-13 19:57:27 +0900

    Update Python Default copy.py

--- Python Default copy.py
+++ Python Default copy.py
@@ -1,4 +1,23 @@
-def cube0to1(cube):
-    mid = (cube + 1.0)/2
-    if ()
-    
+cube = float(input("cube = "))
+epsilon = 0.001
+guess = 0.0
+increment = 0.001
+num_guesses = 0
+limitmax = 1.0
+limitmin = cube
+guess = (cube + limitmax) / 2
+while abs(guess**3 - cube) >= epsilon:
+	if guess**3 >= cube:
+		limitmax = guess
+		guess = (guess + limitmin) / 2
+	else:
+		limitmin = guess
+		guess = (guess + limitmax) / 2
+
+	num_guesses += 1
+	
+	print('num_guesses = ', num_guesses)
+	if abs(guess**3 - cube) >= epsilon:
+		print("Failed on cube root of", cube, guess)
+	else:
+		print(guess, 'is close to the cube root of', cube)
