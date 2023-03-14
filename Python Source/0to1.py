fede2993863bc86555d65d717820f28677d75cd6
Heo Sejin <hsj3925@naver.com>
2023-03-14 17:21:11 +0900

    5

--- Python Source/0to1.py
+++ Python Source/0to1.py
@@ -5,14 +5,14 @@ increment = 0.001
 num_guesses = 0
 limitmax = 1.0
 limitmin = cube
-guess = (cube + limitmax) / 2
+guess = (limitmin + limitmax) / 2
 while abs(guess**3 - cube) >= epsilon:
 	if guess**3 >= cube:
 		limitmax = guess
-		guess = (guess + limitmin) / 2
 	else:
 		limitmin = guess
-		guess = (guess + limitmax) / 2
+  
+	guess = (limitmin + limitmax) / 2
 
 	num_guesses += 1
 	
